9ebb8a45924a54f2ae1a66dc87d774ba2d74c666
felipemaxplay <felipemaxplay@gmail.com>
2021-10-25 19:42:22 -0300

    refatorando appMain

--- src/main .cpp
+++ src/main .cpp
@@ -2,17 +2,23 @@
 #include <stdio.h>
 #include "./headers/menu.h"
 
-int main(int argc, char const *argv[]) {
+void appMain() {
 
     int options = 0;
 
-    do  {
+    do {
+
         menuDisplay();
         printf("Escolha a operação: ");
         scanf("%d", &options);
         printf("\n");
         menuOptions(options);
     } while (options != 0);
-    
+}
+
+int main(int argc, char const *argv[]) {
+
+    appMain();
+
     return 0;
 }
