ce02204e3d618f8d1a224a0be63d3cb85f855ebc
felipemaxplay <felipemaxplay@gmail.com>
2021-10-25 20:53:45 -0300

    insertAluno in nodo complete

--- src/headers/alunos.h
+++ src/headers/alunos.h
@@ -9,3 +9,29 @@ typedef struct nodo {
 } listAlunos;
 
 listAlunos *init = NULL; // variavel que irá conter o endereço do nó inicial.
+
+void insertAluno(listAlunos *point, char namePers[40], int raPers) {
+
+    listAlunos *aux;
+    aux = ( listAlunos * ) malloc(sizeof(listAlunos)); // aloca dinamicamente um novo nó
+    for (int i = 0; i < 40; i++) {
+
+        aux -> name[i] = namePers[i];
+    }
+    aux -> ra = raPers;
+
+    if (point == NULL) { // verifica se a lista está vazia
+        
+        init = aux;
+        init -> pre = NULL;
+        init -> next = NULL;
+    } else { // se a lista não estiver vazia
+        while (point -> next != NULL) {
+
+            point = point -> next;
+        }
+        aux -> pre = point;
+        aux -> next = NULL;
+        point -> next = aux;
+    }   
+}
