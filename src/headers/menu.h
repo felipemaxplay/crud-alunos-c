502d864a5db78f5ca6cd1feb4bf0acff5e38a610
felipemaxplay <felipemaxplay@gmail.com>
2021-10-27 18:20:37 -0300

    ajustes no menu.h

--- src/headers/menu.h
+++ src/headers/menu.h
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include "./menuOptions.h"
 
 void menuDisplay() {
     
-    system("clear");
-    printf("Menu Prinpical\n");
-    printf("--------------\n");
-    printf("[1] - Cadastrar Aluno\n");
-    printf("[2] - Consultar por RA\n");
-    printf("[3] - Listar Todos\n");
-    printf("[4] - Consultar por Nome\n");
-    printf("[5] - Exlcuir por RA\n");
-    printf("[6] - Excluir Todos os Alunos\n");
-    printf("[7] - Exibir Maior e Menor Nome\n");
-    printf("[0] - Sair\n");
+    int options;
+
+    do {
+
+        system("clear");
+        printf("Menu Prinpical\n");
+        printf("--------------\n");
+        printf("[1] - Cadastrar Aluno\n");
+        printf("[2] - Consultar por RA\n");
+        printf("[3] - Listar Todos\n");
+        printf("[4] - Consultar por Nome\n");
+        printf("[5] - Exlcuir por RA\n");
+        printf("[6] - Excluir Todos os Alunos\n");
+        printf("[7] - Exibir Maior e Menor Nome\n");
+        printf("[0] - Sair\n");
+        printf("Escolha a operação: ");
+        scanf("%d", &options);
+        while ((getchar()) != '\n');
+        printf("\n");
+        menuOptions(options);
+    } while(options != 0);
 }
