72572682407babf317f593ce15697c5c5a81f933
felipemaxplay <felipemaxplay@gmail.com>
2021-10-27 18:19:15 -0300

    refeito o main.cpp

--- src/main .cpp
+++ src/main .cpp
@@ -1,26 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "./headers/menu.h"
-#include "./headers/menuOptions.h"
-
-void appMain() {
-
-    int options = 0;
-
-    do {
-
-        menuDisplay();
-        printf("Escolha a operação: ");
-        scanf("%d", &options);
-        while ((getchar()) != '\n');
-        printf("\n");
-        menuOptions(options);
-    } while (options != 0);
-}
 
 int main(int argc, char const *argv[]) {
 
-    appMain();
+    menuDisplay();
 
     return 0;
 }
